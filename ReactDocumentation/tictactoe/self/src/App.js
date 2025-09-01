317599864d0c85f6467c82e9384f4f2b4262bc99
Nd3ritu <nderitumw17@gmail.com>
2025-09-01 21:08:54 +0300

    To handle updating of what happens when sqaure is clicked the sqaures state is defined in the board component which is the parent and the click button function is linked to the board-parent component through the handleclick function which updates the state of the square

--- ReactDocumentation/tictactoe/self/src/App.js
+++ ReactDocumentation/tictactoe/self/src/App.js
@@ -1,44 +1,41 @@
 import { useState } from "react";
 
-function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        setValue('X')
-    }
-    return(
-        <button 
-        className="square"
-        onClick={handleClick}
-        >
-         {value}
-
-         </button>
-
-    ) 
+function Square({value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}> 
+      {value}
+    </button>
+  )
 
 }
 
 export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [squares, setSquares] = useState(Array(9).fill(null)) // created an array with nine elements and sets each of them to null
+
+  function handleClick(i){
+    const nextSquares = squares.slice(); //slice () creates a portion of the original array
+    nextSquares[i] = 'X';
+    setSquares(nextSquares); //responsible for updating the state of the component that has changed as this trigges a re-render of the components that use the squares state (Board) as well as its child components (Square)
+  }
+
     return (
         <>
           <div className="board-row">
-            <Square  value={squares[0]}/>
-            <Square  value={squares[1]}/>
-            <Square value={squares[2]}/>
+            <Square  value={squares[0]} onSquareClick={() => handleClick(0)}/>
+            <Square  value={squares[1]} onSquareClick={ () =>handleClick(1)}/>
+            <Square value={squares[2]} onSquareClick={ () =>handleClick(2)}/>
 
           </div>
           <div className="board-row">
-            <Square value={squares[3]}/>
-            <Square value={squares[4]}/>
-            <Square value={squares[5]}/>
+            <Square value={squares[3]} onSquareClick={ () =>handleClick(3)}/>
+            <Square value={squares[4]} onSquareClick={ () =>handleClick(4)}/>
+            <Square value={squares[5]} onSquareClick={ () =>handleClick(5)}/>
 
           </div>
           <div className="board-row">
-            <Square value={squares[6]}/>
-            <Square value={squares[7]}/>
-            <Square value={squares[8]}/>          
+            <Square value={squares[6]} onSquareClick={ () =>handleClick(6)}/>
+            <Square value={squares[7]} onSquareClick={ () =>handleClick(7)}/>
+            <Square value={squares[8]} onSquareClick={ () =>handleClick(8)}/>          
           </div>       
         </>
     )
