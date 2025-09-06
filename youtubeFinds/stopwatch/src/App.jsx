aae07743fb2f77ca1991bf82186a803da10fd450
Nd3ritu <nderitumw17@gmail.com>
2025-09-06 11:54:55 +0300

    setup time calculations

--- youtubeFinds/stopwatch/src/App.jsx
+++ youtubeFinds/stopwatch/src/App.jsx
@@ -7,6 +7,12 @@ function App() {
   return (
     <>
     <h1>Counter Up Timer</h1>
+    <div>
+      <span>{("0" + Math.floor(time / 60)).slice(-1)}:</span>
+      <span>{("0" + (time % 60)).slice(-2)}:</span>
+      <span>{("0" + Math.floor((time / 60) % 60)).slice(-2)}</span>
+    
+    </div>
       
     </>
   )
