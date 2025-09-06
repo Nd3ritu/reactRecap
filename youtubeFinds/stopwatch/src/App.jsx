313a8636e4e571aa3d992364404ea2b884930a65
Nd3ritu <nderitumw17@gmail.com>
2025-09-06 11:51:38 +0300

    setup the react applicaation

---
+++ youtubeFinds/stopwatch/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [time, setTime] = useState(0)
+
+  return (
+    <>
+    <h1>Counter Up Timer</h1>
+      
+    </>
+  )
+}
+
+export default App
