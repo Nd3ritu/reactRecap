3817ad1db8f8e90f58384904dde8b43d0461d187
Nd3ritu <nderitumw17@gmail.com>
2025-09-11 09:44:22 +0300

    finished updating nested object

--- ReactDocumentation/hooks/src/hooks/useState/updateNestedObjectPart.jsx
+++ ReactDocumentation/hooks/src/hooks/useState/updateNestedObjectPart.jsx
@@ -19,15 +19,57 @@ export default function UpdateNestedObjectPart() {
         })
     }
 
+    function handleTitleChange(e){
+        setUser({
+            ...user,
+            artwork:{
+                ...user.artwork,
+                title: e.target.value
+            }
+        })
+    }
+
+    function handleCityChange(e){
+        setUser({
+            ...user,
+            artwork:{
+                ...user.artwork,
+                city:e.target.value
+            }
+        })
+
+    }
+
+    function handleImageChange(e){
+        setUser({
+            ...user,
+            artwork:{
+                ...user.artwork,
+                image:e.target.value
+            }
+        })
+
+    }
+
     return (
         <>
            <div className="container">
-            <label className="name">Name: 
+            <label className="label">Name: 
             <input value={user.name} onChange={handleNameChange}/>
            </label>
-           <label>Title</label>
-           <label>City</label>
-           <label>Image</label>
+           <label className="label">Title:
+            <input value={user.artwork.title} onChange={handleTitleChange}/>
+           </label>
+           <label className="label">City:
+            <input value={user.artwork.city } onChange={handleCityChange}/>
+           </label>
+           <label className="label">Image:
+            <input value={user.artwork.image} onChange={handleImageChange}/>
+           </label>
+
+           <p>My name is {user.name} and i Live in {user.artwork.city} and my job title is {user.artwork.title}</p>
+
+
            </div>
         
         </>
