de87c9eebc5861e5131885626dbbf9df0b45df96
Nd3ritu <nderitumw17@gmail.com>
2025-10-06 04:29:00 +0300

    delete function works

--- youtubeFinds/taskTracker/src/components/ToDo.jsx
+++ youtubeFinds/taskTracker/src/components/ToDo.jsx
@@ -1,6 +1,13 @@
 import EditTask from "./EditTask"
 
 export default function ToDo({task, index, taskList, setTasklist}) {
+
+    function handleDelete(itemID){
+        let removeItem = taskList.indexOf(task)
+        taskList.splice(removeItem,1)
+        setTasklist((currentList => currentList.filter(todo => todo.id !== itemID)))
+
+    }
     return (
         <>
         <div className="flex flex-col items-start justify-start border bg-white border-gray-300 py-4 px-6 ml-4 my-10 mr-4 p-4 rounded mb-4 w-3/4 max-w-lg">
@@ -12,7 +19,7 @@ export default function ToDo({task, index, taskList, setTasklist}) {
             <p className="text-lg py-2 ">{task.taskDescription}</p>
 
             <div className="w-full flex justify-center">
-                <button className="bg-red-500 text-white text-sm uppercase font-semibold py-1.5 px-4 rounded-lg ">Delete</button>
+                <button className="bg-red-500 text-white text-sm uppercase font-semibold py-1.5 px-4 rounded-lg " onClick={handleDelete} >Delete</button>
             </div>
 
 
