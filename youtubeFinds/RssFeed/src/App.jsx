b713b314f381d6b8fa9e0493b1091094fc5d81be
Nd3ritu <nderitumw17@gmail.com>
2025-10-14 23:46:54 +0300

    successfully fetched the investopedia rss

--- youtubeFinds/RssFeed/src/App.jsx
+++ youtubeFinds/RssFeed/src/App.jsx
@@ -1,7 +1,27 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
+
+import axios from 'axios'
 
 
 function App() {
+  const [articles, setArticles] = useState([])
+
+  console.log(articles)
+
+
+  const getArticles = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/")
+      setArticles(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+    }
+
+    useEffect(() => {
+      getArticles()
+    }, [])
+
 
   return (
     <>
