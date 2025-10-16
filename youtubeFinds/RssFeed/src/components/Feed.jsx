6c96bcbf35483fb79c50572910fe402a0bae186b
Nd3ritu <nderitumw17@gmail.com>
2025-10-16 17:55:57 +0300

    linked to investopedia

--- youtubeFinds/RssFeed/src/components/Feed.jsx
+++ youtubeFinds/RssFeed/src/components/Feed.jsx
@@ -22,7 +22,7 @@ export default function Feed({title, creator,link,pubDate, categories, guid }){
      <div className='p-4 m-4 max-w-sm border border-gray-200 mb-30   rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 hover:scale-105 transition duration-300 ease-in-out ml-10'>
       <h1 className='mb-6 text-xl text-white italic'>{title}</h1>
       <h5 className='text-blue-400 mb-9'>{guid}</h5>
-      <h3 className='mb-9 text-sm hover:text-blue-600 text-gray-500'>{link}</h3>
+      <a href={link} className='mb-9 text-sm hover:text-blue-600 text-gray-500'>{link}</a>
       
       <h5 className='mb-9 text-black border  rounded-lg p-1 bg-purple-300 max-w-fit justify-center ml-25 '>{categories}</h5>
       
