be26c3461919f43186a9f2f415d7b3866135fce3
Nd3ritu <nderitumw17@gmail.com>
2025-10-17 11:57:04 +0300

    added typewriter efffect to title of cards

--- youtubeFinds/RssFeed/src/components/GoodMorning.jsx
+++ youtubeFinds/RssFeed/src/components/GoodMorning.jsx
@@ -3,7 +3,7 @@ import  { TypeAnimation } from 'react-type-animation'
 
 const GoodMorning = () => {
   return (
-    <TypeAnimation speed={30} cursor={false} sequence={['Good Morning Nderitu', 1000]} wrapper="p" repeat={Infinity} className='text-green-400 italic ' />
+    <TypeAnimation speed={30}  cursor={true} sequence={['Good Morning Nderitu...', 1000]} wrapper="p" repeat={Infinity} className='text-green-400 italic ' />
   )
 }
 
--- youtubeFinds/RssFeed/src/components/Investopedia.jsx
+++ youtubeFinds/RssFeed/src/components/Investopedia.jsx
@@ -1,4 +1,4 @@
-
+import { TypeAnimation } from 'react-type-animation'
 import React from 'react'
 
 export default function Investopedia({title, creator,link,pubDate, categories, guid }){
@@ -19,21 +19,29 @@ export default function Investopedia({title, creator,link,pubDate, categories, g
   const date = new Date(pubDate).toLocaleDateString('en-US',formatted)
   return(
     <>
-     <div className='p-4 m-4 max-w-sm border border-gray-200 mb-30   rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 hover:scale-105 transition duration-300 ease-in-out ml-10'>
-      <h1 className='mb-6 text-xl text-white italic'>{title}</h1>
-      <h5 className='text-blue-400 mb-9'>{guid}</h5>
-      <a href={link} className='mb-9 text-sm hover:text-blue-600 text-gray-500'>{link}</a>
+    <a href={link} className='mb-9 text-sm hover:text-blue-600 text-gray-500'>
+      <div className='p-6  max-w-sm border  mb-1   rounded-lg shadow-lg bg-black/80 border-gray-700  backdrop-blur-lg hover:scale-105 transition duration-300 ease-in-out ml-10'>
+      <h1 className='mb-6 text-xl text-white italic text-center'>
+        <TypeAnimation speed={2}  cursor={false} sequence={[title, 1000]} wrapper='h1' repeat={Infinity} className='text-white italic ' />
+        </h1>
+
       
-      <h5 className='mb-9 text-black border  rounded-lg p-1 bg-purple-300 max-w-fit justify-center ml-25 '>{categories}</h5>
+      <div className='relative'>
+        <div className='absolute inset-0 blur shadow-lg bg-purple-400 rounded-lg ml-23  max-w-fit'></div>
+              <h5 className='relative mb-9 text-sm border  rounded-lg p-1 bg-black text-white max-w-fit justify-center ml-25 '>{categories}</h5>
+
+      </div>
       
       <div className=' mb-4 flex justify-between flex-row'>
-      <h5 className='mb-2 text-green-400'>{creator}</h5>
-      <h5 className='mb-2 text-green-200'>{date}</h5>
+      <h5 className='mb-2 text-green-400 text-center align-center justify-center ml-10  '>{date}</h5>
 
       </div>
       
       
      </div>
+
+    </a>
+     
     </>
   )
 }
