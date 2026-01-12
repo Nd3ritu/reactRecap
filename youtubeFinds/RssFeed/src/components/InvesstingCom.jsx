a5bcbf4eef327d456b02352e4714cbdb7d03472c
Nd3ritu <nderitumw17@gmail.com>
2026-01-12 11:55:05 +0300

    formatted pubDAte

--- youtubeFinds/RssFeed/src/components/InvesstingCom.jsx
+++ youtubeFinds/RssFeed/src/components/InvesstingCom.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 const InvesstingCom = ({title, pubDate, link,name }) => {
    const formatted = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+  }
+
+  const date = new Date(pubDate).toLocaleDateString('en-US',formatted)
 
 
   return (
